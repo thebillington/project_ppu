2e08aa8a133db77b32fa50e28cf948c1087137d9
Billy Rebecchi <billy.rebecchi@googlemail.com>
2022-01-30 13:59:36 +0000

    Add extra colours, rename main.py to pong.py, build block breaker game

--- micropython/colour.py
+++ micropython/colour.py
@@ -1,5 +1,5 @@
 hexColourMap = ["#000", "#00f", "#0f0", "#0ff", "#f00", "#f0f", "#ff0", "#fff"]
-binaryColourMap = [0x0000, 0xf800, 0x001f, "011", 0x07E0, "101", "110", 0xffff]
+binaryColourMap = [0x0000, 0xf800, 0x001f, 0xf81f, 0x07e0, 0xffe0, 0x07ff, 0xffff]
 class Colour:
 
     def __init__(self, colour_int):
@@ -14,5 +14,8 @@ class Colour:
 Colour.BLACK = Colour(0)
 Colour.BLUE = Colour(1)
 Colour.GREEN = Colour(2)
+Colour.TURQUOISE = Colour(3)
 Colour.RED = Colour(4)
+Colour.PURPLE = Colour(5)
+Colour.YELLOW = Colour(6)
 Colour.WHITE = Colour(7)
